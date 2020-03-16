e5edb9b6bf7086d65a3a34a1e6e436e1eeb9ffe9
secondriver <413733843@qq.com>
2020-03-16 23:18:09 +0800

    add

--- java_image_server/src/main/java/api/ImageServlet.java
+++ java_image_server/src/main/java/api/ImageServlet.java
@@ -1,5 +1,7 @@
 package api;
 
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 import dao.Image;
 import dao.ImageDao;
 import org.apache.commons.fileupload.FileItem;
@@ -7,12 +9,10 @@ import org.apache.commons.fileupload.FileItemFactory;
 import org.apache.commons.fileupload.FileUploadException;
 import org.apache.commons.fileupload.disk.DiskFileItemFactory;
 import org.apache.commons.fileupload.servlet.ServletFileUpload;
-
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.awt.*;
 import java.io.File;
 import java.io.IOException;
 import java.text.SimpleDateFormat;
@@ -34,13 +34,47 @@ public class ImageServlet extends HttpServlet {
      */
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        // req  请求   方法 url，各种 header，body
-        // resp 响应   状态码，各种 header，body
-        resp.setStatus(200);
-        // 把 hello 字符串放到 http 响应的 body 中
-        resp.getWriter().write("hello");
-        // 修改 webapp/WEB-INF/web.xml 把新创建的 servelet 注册进去
+        // 考虑查看所有图片属性和查看指定图片属性
+        // 通过是否 URL 中带有 imageId 参数来进行区分
+        // 存在 imageId 查看指定图片属性，否则就查看所有图片属性
+        // 如果URL /image?imageId=100
+        // imageId 的值就是 "100"
+        // 如果 URL 中不存在 imageId 那么返回 null
+        String imageId = req.getParameter("imageId");
+        if (imageId == null || imageId.equals("")){
+            // 查看所有图片属性
+            selectAll(req,resp);
+        }else {
+            // 查看指定图片
+            selectOne(imageId,resp);
+        }
     }
+    private void selectAll(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+        resp.setContentType("application/json; charset=utf-8");
+        // 1.创建一个 ImageDao 对象，并查找数据库
+        ImageDao imageDao = new ImageDao();
+        List<Image> images = imageDao.selectAll();
+        // 2.把查找到的结果转成 JSON 格式的字符串，并且写回给 resp 对象
+        Gson gson = new GsonBuilder().create();
+        //    jsonData 就是一个 json 格式的字符串，和之前约定的格式是一样的
+        //    重点是下面这行代码，gson 帮我们完成了大量的格式转换
+        //    只要把相关的字段都约定成统一的命名，下面的操作就可以一步到位的完成转换
+        String jsonData = gson.toJson(images);
+        resp.getWriter().write(jsonData);
+    }
+    private void selectOne(String imageId, HttpServletResponse resp) throws IOException {
+        resp.setContentType("application/json; charset=utf-8");
+        //1. 创建 ImageDao 对象
+        ImageDao imageDao = new ImageDao();
+        Image image = imageDao.selectOne(Integer.parseInt(imageId));
+        // 2. 使用 gson 把查到的数据转成 json 格式，并写回给响应对象
+        Gson gson = new GsonBuilder().create();
+        String jsonData = gson.toJson(image);
+        resp.getWriter().write(jsonData);
+    }
+
+
+
 
     /**
      *上传图片
@@ -83,7 +117,8 @@ public class ImageServlet extends HttpServlet {
         image.setUploadTime(simpleDateFormat.format(new Date()));
         image.setContentType(fileItem.getContentType());
         // 自己构造一个路径来保存
-        image.setPath("./image/" + image.getImageName());
+        //                        加入时间戳使文件路径唯一
+        image.setPath("./image/" + System.currentTimeMillis() + "_" + image.getImageName());
         //  MD5 暂时不考虑
         image.setMd5("11223344");
         // 存到数据库中
@@ -115,6 +150,27 @@ public class ImageServlet extends HttpServlet {
      */
     @Override
     protected void doDelete(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        super.doDelete(req, resp);
+        // 1. 先获取到请求中的 imageId
+        String imageId = req.getParameter("imageId");
+        if (imageId == null || imageId.equals("")){
+            resp.setStatus(200);
+            resp.getWriter().write("{\"ok\":false,\"reason\":\"解析请求失败\"}");
+            return;
+        }
+        // 2. 创建 ImageDao 对象，查看到该图片对象对应的相关属性（这是为了只知道图片的对应文件路径）
+        ImageDao imageDao = new ImageDao();
+        Image image = imageDao.selectOne(Integer.parseInt(imageId));
+        if (image == null){
+            // 此时请求中传入的 id 在数据库中不存在
+            resp.setStatus(200);
+            resp.getWriter().write("{\"ok\":false,\"reason\":\"imageId在数据库中不存在\"}");
+        }
+        // 3. 删除本地库中的记录
+        imageDao.delete(Integer.parseInt(imageId));
+        // 4. 删除本地磁盘文件
+        File file = new File(image.getPath());
+        file.delete();
+        resp.setStatus(200);
+        resp.getWriter().write("{\"ok\":ture}");
     }
 }
